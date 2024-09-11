ceb1f70cc9aa2b1d09a9b9fa1a81c41bcdaea7d3
KrishGitss <krish.thakore04@gmail.com>
2024-09-11 23:15:56 +0400

    Create Lab 1, Q12.c

---
+++ Lab 1, Q12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    printf("ASCII value of the character is %c = %d", c, c);
+
+    return 0;
+}
