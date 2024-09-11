b0fb84dce7c443bd2144ca3e582e7df1d5b16c49
KrishGitss <krish.thakore04@gmail.com>
2024-09-11 23:17:56 +0400

    Lab 1, Q13.c

---
+++ L13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, r;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+    {
+    printf("%d is even.", n);
+    }
+    else 
+    {
+    printf("%d is odd.", n);
+    }
+
+    return 0;
+
+}
