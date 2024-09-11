389dc0e00be5be15cb4fa4790afd824e56803930
KrishGitss <krish.thakore04@gmail.com>
2024-09-11 23:34:15 +0400

    Create Lab 2, Q22.c

---
+++ Lab 2, Q22.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{   
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+    printf("Enter an operator (+ - * /): ");
+    scanf("%c", &operator);
+
+    printf("Num1: ");
+    scanf("%lf", &num1);
+
+    printf("Num2: ");
+    scanf("%lf", &num2);
+
+    switch(operator)
+    {
+    case '+':
+    result = num1 + num2;
+        printf("Result: %.2lf", result);
+        break;
+
+    case '-':
+    result = num1 - num2;
+        printf("Result: %.2lf", result);
+        break;
+    
+    case '*':
+    result = num1 * num2;
+        printf("Result: %.2lf", result);
+        break;
+
+    case '/':
+    result = num1 / num2;
+        printf("Result: %.2lf", result);
+        break;
+    
+    default:
+        printf("%c is not valid", operator);
+        break;
+    }
+    return 0;
+}
