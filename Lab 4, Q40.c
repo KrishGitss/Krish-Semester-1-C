22cc86e1bd42327eaa7282271a502f358d7db5d0
KrishGitss <krish.thakore04@gmail.com>
2024-09-12 11:03:26 +0400

    Create Lab 4, Q40.c

---
+++ Lab 4, Q40.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0; 
+    
+    do {
+        printf("Input a number (input a negative number to stop): ");
+        scanf("%d", &num);
+
+        if (num >= 0) {
+            sum += num; 
+        }
+
+    } while (num >= 0);  
+
+    printf("Sum of entered numbers: %d\n", sum);
+
+    return 0; 
+}
