600634a202580872410c36ab52e6c49215aa0586
KrishGitss <krish.thakore04@gmail.com>
2024-11-28 13:22:59 +0530

    Create Lab 6, Q61.c

---
+++ Lab 6, Q61.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, swapped;
+
+    for (i = n - 1; i >= 0; i--) {
+        swapped = 0;
+
+        for (j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
